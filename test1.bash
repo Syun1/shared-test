5dbe9e49f04238c79459d6e6e16de76e093fff9b
Syunichi Ohno <a1607so@aiit.ac.jp>
2016-06-12 10:05:16 +0900

    travis CI test

--- test1.bash
+++ test1.bash
@@ -16,12 +16,9 @@ exit 1
 echo " 6 " > $tmp-ans         #予想解
 ./16745107 18 12 > $tmp-out1  #実行結果その1
 ./16745107 12 18 > $tmp-out2  #実行結果その2
-./16745107 20 10 > $tmp-out3  #実行結果その3
 
 diff $tmp-ans $tmp-out1 || ERROR_EXIT "TEST1"
 diff $tmp-ans $tmp-out2 || ERROR_EXIT "TEST2"
-diff $tmp-ans $tmp-out3 || ERROR_EXIT "TEST3"
-
 echo OK
 rm -f $tmp-*
 exit 0
